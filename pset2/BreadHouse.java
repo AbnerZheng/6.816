86490916970bf060949267589f6cf6fb641ac859
Gina Yuan <gina.yuan@gmail.com>
2017-02-26 23:24:18 -0500

    BreadHouse implementation

--- pset2/BreadHouse.java
+++ pset2/BreadHouse.java
@@ -34,15 +34,19 @@ interface BreadBox {
  */
 public class BreadHouse {
    /*
-    * TODO: Add fields to keep track of kitchen table notes here!
     * All fields accessed by both threads must be Atomic classes from
     * java.util.concurrent.atomic
     */
-    private AtomicBoolean exampleField;
+    private AtomicBoolean A1;
+    private AtomicBoolean A2;
+    private AtomicBoolean S1;
+    private AtomicBoolean S2;
     
     public BreadHouse() {
-        // TODO: Implement me!
-        exampleField = new AtomicBoolean(false);
+        A1 = new AtomicBoolean(false);
+        A2 = new AtomicBoolean(false);
+        S1 = new AtomicBoolean(false);
+        S2 = new AtomicBoolean(false);
     }
     
    /*
@@ -58,7 +62,28 @@ public class BreadHouse {
     *            A reference to the breadbox which must be refilled.
     */
     public void alex(BreadBox box) {
-        // TODO: Implement me!
+        // When I get home, leave a note A1 saying "Alex came home".
+        A1.set(true);
+
+        // If I do see Sam’s note S2, leave a note A2 saying ”Alex avocado!”
+        // else remove any A2 notes.
+        if (S2.get()) {
+            A2.set(true);
+        } else {
+            A2.set(false);
+        }
+
+        // Wait at the table as long as S1 is present and either both A2 and
+        // S2 are present or both are not.
+        while (S1.get() && !(A2.get() ^ S2.get())) {};
+
+        // If I see that there is no bread, go buy a new loaf of bread.
+        if (box.isEmpty()) {
+            box.addLoaf();
+        }
+
+        // Remove my note A1.
+        A1.set(false);
 	}
 	
    /*
@@ -74,6 +99,27 @@ public class BreadHouse {
     *            A reference to the breadbox which must be refilled.
     */
     public void sam(BreadBox box) {
-        // TODO: Implement me!
+        // When I get home, leave a note S1 saying ”Sam came home”.
+        S1.set(true);
+
+        // If I don’t see Alex’s note A2, leave a note S2 saying ”Sam salami!”
+        // else remove any S2 notes.
+        if (!A2.get()) {
+            S2.set(true);
+        } else {
+            S2.set(false);
+        }
+
+        // Wait at the table as long as A1 is present and exactly one of A2
+        // or S2 are present.
+        while (A1.get() && (A2.get() ^ S2.get())) {};
+
+        // If I see that there is no bread, go buy a new loaf of bread.
+        if (box.isEmpty()) {
+            box.addLoaf();
+        }
+
+        // Remove my note S1.
+        S1.set(false);
 	}
 }
