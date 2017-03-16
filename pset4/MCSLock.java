fe433957f4165b66caf8f96b24a63258f7eb398d
Gina Yuan <gina.yuan@gmail.com>
2017-03-16 00:35:33 -0400

    Init pset4

---
+++ pset4/MCSLock.java
@@ -0,0 +1,36 @@
+import java.util.concurrent.atomic.*;
+
+public class MCSLock implements Lock {
+    /**
+    * Add your fields here
+    */
+
+    public MCSLock() {
+        //TODO: Implement me!
+    }
+
+    public void lock() {
+        //TODO: Implement me!
+    }
+
+    public void unlock() {
+        //TODO: Implement me!
+    }
+    
+    /**
+     * Checks if the calling thread observes another thread concurrently
+     * calling lock(), in the critical section, or calling unlock().
+     * 
+     * @return
+     *          true if another thread is present, else false
+     */
+    public boolean isContended() {
+        //TODO: Implement me!
+    }
+
+    class QNode {
+        /**
+        * Add your fields here
+        */
+    }
+}
