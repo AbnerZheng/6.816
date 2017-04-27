fe6c47d9ea024e26940583c912fcedf19801493c
Gina Y Yuan <gyuan@EECS-ATH-34.MIT.EDU>
2017-04-27 15:18:28 -0400

    Update partb.sh

--- pset5/tests/partb.sh
+++ pset5/tests/partb.sh
@@ -1,10 +1,13 @@
 add 6.816
+TESTNUM=0
 for RHO in 0.5 0.75 0.9
 do
-    cqsub python tests/batch_test.py b 0.09 0.01 $RHO
+    cqsub python tests/batch_test.py b $TESTNUM 0.09 0.01 $RHO
+    ((TESTNUM+=1))
 done
 
 for RHO in 0.5 0.75 0.9
 do
-    cqsub python tests/batch_test.py b 0.45 0.05 $RHO
+    cqsub python tests/batch_test.py b $TESTNUM 0.45 0.05 $RHO
+    ((TESTNUM+=1))
 done
